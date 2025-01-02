754f270cdb877f7138f60ce8c6d396025b756c8e
defcon_1 <n.a.ozerov@gmail.com>
2025-01-02 19:07:52 +0300

    WindowInterface.py
    Update of WindowInterface. Now it writes a config file and then opens other window (see AdjustableWindow.py)

--- WindowInterface.py
+++ WindowInterface.py
@@ -2,6 +2,7 @@
 import pygame, sys, pygame_widgets
 from pygame_widgets.textbox import TextBox
 from pygame_widgets.button import Button
+from os import system
 pygame.init()
 
 #colors
@@ -10,6 +11,7 @@ GRAY = (75, 75, 75)
 #setup window
 pygame.display.set_caption("Set screen size")
 setupScreen = pygame.display.set_mode(size = (320, 200))
+output = open('config.txt', "a")
 
 screenSize = ()
 
@@ -23,9 +25,13 @@ def Close(x):
     global screenSize
     if len(screenSize) != 0:
         print(f'Size {screenSize} confirmed')
+        output.write('.' + str(screenSize)[1:-1].replace(',', ''))
+        output.close()
+        system("AdjustableWindow.py")
         quit()
     else:
         print(f'Select screen size first')
+
 set640x480 = Button(setupScreen, x = 0, y = 0, width = 100, height = 50, onClick=lambda : returnSize(640, 480), text = '640x480')
 set800x600 = Button(setupScreen, x = 0, y = 50, width = 100, height = 50, onClick=lambda : returnSize(800, 600), text = '800x600')
 set1024x768 = Button(setupScreen, x = 0, y = 100, width = 100, height = 50, onClick=lambda : returnSize(1024, 768), text = '1024x768')
@@ -35,7 +41,8 @@ set1920x1080 = Button(setupScreen, x = 100, y = 100, width = 100, height = 50, o
 confirmButton = Button(setupScreen, x = 220, y = 150, width = 100, height = 50, onClick=lambda : Close(1), text = 'Confirm')
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
     setupScreen.fill(GRAY)
     pygame_widgets.update(pygame.event.get())
     pygame.display.update()
